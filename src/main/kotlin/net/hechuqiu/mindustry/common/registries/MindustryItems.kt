1d2fdb02fd33566edf803ab8997a0983ca569cd0
HeChu <2296282722@qq.com>
2025-04-14 01:04:00 +0800

    添加BoundingBlockItem类，解决 issue #27
    - 创建BoundingBlockItem类，实现IBoundingBlock接口的放置逻辑
    - 相应地修改了MindustryItems注册表
    
    fixes #27

--- src/main/kotlin/net/hechuqiu/mindustry/common/registries/MindustryItems.kt
+++ src/main/kotlin/net/hechuqiu/mindustry/common/registries/MindustryItems.kt
@@ -1,6 +1,7 @@
 package net.hechuqiu.mindustry.common.registries
 
 import net.hechuqiu.mindustry.Mindustry
+import net.hechuqiu.mindustry.common.item.BoundingBlockItem
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.neoforged.neoforge.registries.DeferredRegister
@@ -17,6 +18,6 @@ object MindustryItems {
     }
 
     val THREE_BLOCK_HIGH_BLOCK = REGISTRY.register("three_block_high") { registryName ->
-        BlockItem(MindustryBlocks.THREE_BLOCK_HIGH_BLOCK, Item.Properties())
+        BoundingBlockItem(MindustryBlocks.THREE_BLOCK_HIGH_BLOCK, Item.Properties())
     }
 }
